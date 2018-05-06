7cb512c686877850b5cca314eb26d43957a43c41
wanghairong-i <3189578410@qq.com>
2018-05-06 19:39:06 +0800

    add utils and up request.ts

---
+++ src/utils/commonModel.ts
@@ -0,0 +1,30 @@
+import { ReduxAction, DvaModelReducers } from 'interfaces/index'
+import _modelExtend from 'dva-model-extend'
+
+interface CommonModel {
+  reducers: DvaModelReducers
+}
+
+const commonModel: CommonModel = {
+  reducers: {
+    updateState(state: object, { payload }: ReduxAction) {
+      return {
+        ...state,
+        ...payload,
+      }
+    },
+    error(state: object, { payload }: ReduxAction) {
+      return {
+        ...state,
+        error: payload,
+      }
+    },
+  },
+}
+
+const modelExtend = (model) => _modelExtend(commonModel, model);
+
+export {
+  modelExtend,
+  commonModel,
+}
