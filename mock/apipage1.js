8dcc48800d0fc1f84a7d363a6eb540a69b8f5ab1
wanghairong-i <3189578410@qq.com>
2018-05-06 19:37:16 +0800

    add mock file

---
+++ mock/apipage1.js
@@ -0,0 +1,17 @@
+// const Mock = require('mockjs');
+
+// const data = Mock.mock({
+//   'list|11': [
+//     {
+//       'id|+1': 1,
+//       name: '@CNAME',
+//     }
+//   ]
+// });
+
+module.exports = {
+
+  // [`/apipage1`]: data.list,
+  [`/apipage1`]: [1,2,3],
+
+}
